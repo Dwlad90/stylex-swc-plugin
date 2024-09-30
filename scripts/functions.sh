664f84ee664277ff4415f8606ac9e49ca3e38063
Dwlad90 <dwlad90@gmail.com>
2024-09-30 22:42:25 +0300

    chore: add script to bump pre-prelease version

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -11,19 +11,27 @@ increment_version() {
   local version="$1"
   # shellcheck disable=SC3043
   local release_type="$2"
+  # shellcheck disable=SC3043
+  local pre_release_type="$3"
 
   if [ -z "$release_type" ]; then
-    printf "Error: No release type specified. Please provide a release type (major, minor, patch).\n\n"
+    echo "Error: No release type specified. Please provide a release type (major, minor, patch)."
     exit 1
   fi
 
   if [ "$release_type" != "major" ] && [ "$release_type" != "minor" ] && [ "$release_type" != "patch" ]; then
-    printf "Error: Invalid release type specified. Please provide a valid release type (major, minor, patch). Passed: %s.\n\n" "$release_type"
+    echo "Error: Invalid release type specified. Please provide a valid release type (major, minor, patch)."
     exit 1
   fi
 
-  IFS='.' read -r major minor patch <<EOF
-  $version
+  # Split version and pre-release identifier
+  read -r core_version pre_release <<EOF
+$(echo "$version" | tr '-' ' ')
+EOF
+
+  # Split core version into major, minor, and patch
+  read -r major minor patch <<EOF
+$(echo "$core_version" | tr '.' ' ')
 EOF
 
   case $release_type in
@@ -31,21 +39,43 @@ EOF
       major=$((major + 1))
       minor=0
       patch=0
+      pre_release=""
       ;;
     minor)
       minor=$((minor + 1))
       patch=0
+      pre_release=""
       ;;
     patch)
       patch=$((patch + 1))
+      pre_release=""
       ;;
   esac
+
+  if [ -n "$pre_release_type" ]; then
+    if [ -z "$pre_release" ]; then
+      pre_release="$pre_release_type.1"
+    else
+      read -r current_pre_release_type current_pre_release_version <<EOF
+$(echo "$pre_release" | tr '.' ' ')
+EOF
+      if [ "$current_pre_release_type" = "$pre_release_type" ]; then
+        current_pre_release_version=$((current_pre_release_version + 1))
+        pre_release="$pre_release_type.$current_pre_release_version"
+      else
+        pre_release="$pre_release_type.1"
+      fi
+    fi
+  fi
 
   # Trim any leading or trailing whitespace from the version components
   major=$(echo "$major" | xargs)
   minor=$(echo "$minor" | xargs)
   patch=$(echo "$patch" | xargs)
 
-
-  echo "$major.$minor.$patch"
+  if [ -z "$pre_release" ]; then
+    echo "$major.$minor.$patch"
+  else
+    echo "$major.$minor.$patch-$pre_release"
+  fi
 }

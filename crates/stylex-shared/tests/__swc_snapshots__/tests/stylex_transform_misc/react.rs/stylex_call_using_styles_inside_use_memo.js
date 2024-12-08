a327bed44da04175e1f6819a61cb9a31bf29ca3c
Dwlad90 <dwlad90@gmail.com>
2024-12-08 18:45:53 +0200

    test(stylex_transform_misc): add test to using props into useMemo

---
+++ crates/stylex-shared/tests/__swc_snapshots__/tests/stylex_transform_misc/react.rs/stylex_call_using_styles_inside_use_memo.js
@@ -0,0 +1,21 @@
+//__stylex_metadata_start__[{"class_name":"x1e2nbdu","style":{"rtl":null,"ltr":".x1e2nbdu{color:red}"},"priority":3000}]__stylex_metadata_end__
+import _inject from "@stylexjs/stylex/lib/stylex-inject";
+var _inject2 = _inject;
+import stylex from 'stylex';
+import { useMemo } from 'react';
+_inject2(".x1e2nbdu{color:red}", 3000);
+const styles = {
+    selected: {
+        color: "x1e2nbdu",
+        $$css: true
+    }
+};
+export default function MyComponent() {
+    const isSelected = true;
+    const innerComponent = useMemo(()=>{
+        return <Component {...stylex.props(isSelected && styles.selected)}/>;
+    }, [
+        isSelected
+    ]);
+    return innerComponent;
+}
---
+++ crates/stylex-shared/tests/stylex_transform_misc/react.rs
@@ -0,0 +1,38 @@
+use stylex_shared::{shared::structures::plugin_pass::PluginPass, StyleXTransform};
+use swc_core::ecma::{
+  parser::{Syntax, TsSyntax},
+  transforms::testing::test,
+};
+
+test!(
+  Syntax::Typescript(TsSyntax {
+    tsx: true,
+    ..Default::default()
+  }),
+  |tr| StyleXTransform::new_test_force_runtime_injection(
+    tr.comments.clone(),
+    PluginPass::default(),
+    None
+  ),
+  stylex_call_using_styles_inside_use_memo,
+  r#"
+    import stylex from 'stylex';
+    import { useMemo } from 'react';
+
+    const styles = stylex.create({
+      selected: {
+        color: 'red',
+      },
+    });
+
+    export default function MyComponent() {
+      const isSelected = true;
+
+      const innerComponent = useMemo(() => {
+        return <Component {...stylex.props(isSelected && styles.selected)} />
+      }, [isSelected]);
+
+      return innerComponent;
+    }
+"#
+);

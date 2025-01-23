77c00a3979dc6ad9bff532075513e03cc4f477b9
Matt Styles <matt@personalurban.com>
2025-01-23 11:42:11 +0000

    feat(stylex_compiler_rs): update use project log formatter

--- crates/stylex-shared/src/shared/utils/log/logger.rs
+++ crates/stylex-shared/src/shared/utils/log/logger.rs
@@ -1,24 +1,15 @@
 use env_logger::{Builder, WriteStyle};
 use log::LevelFilter;
-use std::io::Write;
 use std::sync::Once;
+
+use super::formatter::log_formatter;
 
 static INIT: Once = Once::new();
 
 pub fn initialize() {
   INIT.call_once(|| {
     Builder::new()
-      .format(|buf, record| {
-        writeln!(
-          buf,
-          "[{}] {} [{}:{}] - {}",
-          record.level(),
-          chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-          record.file().unwrap_or("unknown"),
-          record.line().unwrap_or(0),
-          record.args()
-        )
-      })
+      .format(log_formatter)
       .filter_level(LevelFilter::Warn)
       .parse_env("STYLEX_DEBUG") // Allow override via environment variable
       .write_style(WriteStyle::Always)

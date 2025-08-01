84c37bdffcce93838685ba3c01e1c9c8efb5c9ae
Dwlad90 <dwlad90@gmail.com>
2025-08-01 13:26:50 +0300

    fix: unplugin version

--- packages/unplugin/src/farm.ts
+++ packages/unplugin/src/farm.ts
@@ -1,4 +1,7 @@
 import { createFarmPlugin } from 'unplugin';
 import { unpluginFactory } from './index';
+import type { UnpluginStylexRSOptions } from './index';
+import type { JsPlugin } from '@farmfe/core';
 
-export default createFarmPlugin(unpluginFactory);
+const plugin: (options?: UnpluginStylexRSOptions) => JsPlugin = createFarmPlugin(unpluginFactory);
+export default plugin;

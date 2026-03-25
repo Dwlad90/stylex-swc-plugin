5c2c908ddf5a398e4f1d80587c5621c186c4c934
Dwlad90 <dwlad90@gmail.com>
2026-03-25 22:54:51 +0200

    wip: v1

---
+++ crates/stylex-macros/src/lib.rs
@@ -0,0 +1,14 @@
+pub mod collection_macros;
+pub mod conversion_macros;
+pub mod panic_macros;
+pub mod stylex_error;
+
+#[cfg(test)]
+mod tests {
+  use ctor::ctor;
+
+  #[ctor]
+  fn init_logger() {
+    pretty_env_logger::formatted_builder().init();
+  }
+}
